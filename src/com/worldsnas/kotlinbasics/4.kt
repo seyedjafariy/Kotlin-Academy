a846928f9d5b984e94d4316315458880ef44bd02
worldsnas <worldsnas@gmail.com>
2019-10-16 23:06:22 +0330

    a few steps to 4 still way to go

--- src/com/worldsnas/kotlinbasics/4.kt
+++ src/com/worldsnas/kotlinbasics/4.kt
@@ -2,23 +2,158 @@ package com.worldsnas.kotlinbasics
 
 import kotlin.random.Random
 
+
+
+
+
+
+
+
+
 //args vs parameters
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//now we go fundamental all the way!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ok time too see some more functions
+
+
+
+
+
+
+
+
+
+
+
+
 
 //type args and type param
 private fun functionWithParameter(first: Int, second: String) {
     println("first parameter= $first, and second parameter=$second")
 }
 
-//we have a type parameter T
-private open class NewList<T>
 
-//we define Int as type argument
-private class EmptyList : NewList<Int>()
+
+
+
+
+
+
+
+
+
+//first and second are called parameters
+//we will see what is called an argument in just a bit
+//but before that let's see what else is called parameter
+
+
+
+
+
+
+
+
+
 
 
 private fun <T> functionWithTypeParam(obj : T) {
     println(obj)
 }
+
+
+
+
+
+
+
+
+//we call this "T" a type parameter because we can pass a type instead of a value to it!
+//easy peasy!
+
+
+
+
+
+
+
+
+
+
+
+
+//we have parameters in class definitions too, so not just functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+private open class NewList<T>
+
+
+
+
+
+
+
+
+
+
+
+//class NewList has a type parameter "T"
+//time to reveal arguments!
+
+
+
+
+
+
+
+
+
+
+
+//we define Int as type argument
+private class EmptyList : NewList<Int>()
+
+
+
 
 fun main() {
     val firstArg = Random.nextInt()
