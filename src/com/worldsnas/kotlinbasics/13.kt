e05936f7b8bec82126a9f0be44a859f0d2e47d2a
worldsnas <worldsnas@gmail.com>
2019-10-17 11:44:00 +0330

    finalized 13

--- src/com/worldsnas/kotlinbasics/13.kt
+++ src/com/worldsnas/kotlinbasics/13.kt
@@ -1,5 +1,61 @@
 package com.worldsnas.kotlinbasics
 
-fun test(){
 
+
+
+
+
+
+
+
+
+
+//returns from lambdas labels and anonymous
+
+
+
+
+
+
+
+
+
+
+private fun higherOrderFunction(call: (String) -> Boolean) {
+    val list = listOf<Int>(1, 2, 3, 4)
+    val mutableList = mutableListOf<Int>()
+
+    list.forEach {
+
+        if (call(it.toString())) {
+            return
+        } else {
+            mutableList.add(it)
+        }
+    }
+
+
+
+    println("function done with list=$mutableList")
+}
+
+
+fun main() {
+    higherOrderFunction {
+        it == "1"
+    }
+
+
+
+
+
+
+
+
+
+    //same higher order function but with anonymous function as argument
+
+//    higherOrderFunction(fun(String): Boolean {
+//        return false
+//    })
 }
