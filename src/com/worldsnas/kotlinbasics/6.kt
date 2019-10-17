27dd99e72182f0703d521ec9e7834922b60941fb
worldsnas <worldsnas@gmail.com>
2019-10-17 09:25:20 +0330

    formatted 6 and added generic producer

--- src/com/worldsnas/kotlinbasics/6.kt
+++ src/com/worldsnas/kotlinbasics/6.kt
@@ -1,6 +1,43 @@
 package com.worldsnas.kotlinbasics
 
+
+
+
+
+
+
+
 //consumer and producer functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+//another easy but important concept about functions
+//they basically mean input and out of a function
+//so we call a function a consumer when it takes a input for the class
+//as obvious we call a function producer when returns a value
+//as an example
+
+
+
+
+
+
+
+
+
+
+
+
 
 class ConsumerAndProducerFunctions(){
 
@@ -12,9 +49,42 @@ class ConsumerAndProducerFunctions(){
     }
 }
 
+
+
+
+
+
+
+
+//easy peasy!
+
+
+
+
+
+
+
+
+
 class ConsumerWithGenerics<in T> {
 
     fun take(item : T){
 
     }
 }
+
+abstract class ProducerWithGenerics<out T> {
+
+    abstract fun take(): T
+}
+
+
+
+
+
+
+
+
+
+//same class but with generic input function and invariant consumer
+//and the generic producer function
