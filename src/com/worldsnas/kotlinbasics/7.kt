417c4dacbb9410276b50fdf4bcbac3e2725504f5
worldsnas <worldsnas@gmail.com>
2019-10-17 09:39:06 +0330

    finalized 7

--- src/com/worldsnas/kotlinbasics/7.kt
+++ src/com/worldsnas/kotlinbasics/7.kt
@@ -1,39 +1,150 @@
 package com.worldsnas.kotlinbasics
 
+
+
+
+
+
+
+
+
 //static functions in kotlin
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //what is a static function really?
-//static function is a function that does not need a context to run (not android context)
+//static function is a function that does not need a context to run (not android context!!!)
 //that means they are out of scope of a class
 //but how do we create a static function in kotlin?
+//easy answer is we don't! because in kotlin there is nothing as static function
 
 
 
-//first and the most idiomatic way in kotlin to create a static function is
+
+
+
+
+
+
+
+
+
+//first and the most idiomatic (kotlin-ish) way in kotlin to create a static function is
 //package level functions
 
 
-private fun thisIsAStaticFunction(){
-    println("called the static function without creating a class")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+private fun thisIsAPackageLevelFunction(){
+    println("called the PackageLevel function without creating a class")
 }
 
+
+
+
+
+
+
+
+
+//we call them package level because they are not bound to a class
+// the only thing they need is just a package!
+//to use this function you can import and just call it anywhere in your project
+//these type are the preferred ones
+//let's see some more
+
+
+
+
+
+
+
+
+
+
+
 //another ways is to create a function through object keyword
+
+
+
+
+
+
+
+
+
+
+
+
 
 private object SingleObject{
 
     private fun functionInsideObject(){
-        println("object static function has been called")
+        println("object function has been called")
     }
 
 }
 
+
+
+
+
+
+
+
+//when using they look very similar to static functions
 //but functions inside object are not really static
-//the reason for that is because your are calling the function on object single instance rather than
-//a real static function which is out of scope of an object
+//the reason for that is because your are calling the function on object
+//single instance rather than a real static function which is out of scope of an object
+
+
+
+
+
+
+
+
+
 
 
 
 //last but not least
 //companion objects inside classes
+
+
+
+
+
+
+
+
+
 
 private class ClassWithCompanion{
 
@@ -45,10 +156,27 @@ private class ClassWithCompanion{
     }
 }
 
-//same story here we are not creating a real static function here but rather a new object called
-//companion
 
-//companion object functions are very usefull when we need a factory for our class
+
+
+
+
+
+//same story here we are not creating a real static function for ClassWithCompanion
+//but rather a new object called companion which has only one instance
+//remember if you need to call you companion object from java code as static functions
+//you need to add the @JvmStatic
+//but what is the use case?
+//companion object functions are very useful when we need a factory for our class
+
+
+
+
+
+
+
+
+
 
 
 private class Shape private constructor(
@@ -66,8 +194,9 @@ private class Shape private constructor(
 }
 
 
+
 fun main() {
-    ClassWithCompanion.staticFunctionFromCompanionObject()
+    Shape.createSquare(50)
 }
 
-
+//you can see we have the power of factory method for our class using companion objects
